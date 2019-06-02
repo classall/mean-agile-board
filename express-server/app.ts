17e94117c1a22447349a865117a9e0d274cb4edb
classall <corentin.lassalle@outlook.fr>
2019-06-02 16:21:43 +0200

    - Add JWT auth routes

--- express-server/app.ts
+++ express-server/app.ts
@@ -1,14 +1,49 @@
 import bodyParser = require('body-parser');
 import express = require('express');
+import passport = require('passport');
+import passportJWT = require('passport-jwt');
+import { Strategy as LocalStrategy } from 'passport-local';
+
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Passport configuration
+const mockUser = {
+  id: 1,
+  email: 'testuser@yopmail.com',
+  password: 'test'
+};
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password'
+},
+  (email, password, next) => {
+    if (email === mockUser.email && password === mockUser.password) {
+      return next(null, mockUser, { message: 'Logged In Successfully' });
+    } else {
+      return next(null, false, { message: 'Incorrect email or password.' });
+    }
+  }
+));
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const JWTStrategy = passportJWT.Strategy;
+passport.use(new JWTStrategy({
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: 'jwt_secret'
+},
+  (jwtPayload, next) => {
+    if (jwtPayload.id === mockUser.id) {
+      return next(null, mockUser);
+    }
+    return next(null);
+  }
+));
+
+app.use('/', require('./modules/auth/auth.router'));
 
 const port = process.env.PORT || '3000';
 app.set('port', port);
---
+++ express-server/modules/auth/auth.router.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+import jwt = require('jsonwebtoken');
+import passport = require('passport');
+
+const router = Router();
+
+router.post('/api/login',
+  (req, res, next) => {
+    passport.authenticate('local', { session: false },
+      (err, user, info) => {
+        if (err || !user) {
+          return res.status(400).json({
+            message: info.message ? info.message : 'Something is not right',
+            error: err,
+            user,
+          });
+        }
+        req.login(user, { session: false }, (loginError) => {
+          if (loginError) {
+            res.send(loginError);
+          }
+          // generate a signed son web token with the contents of user object and return it in the response
+          const token = jwt.sign(user, 'jwt_secret');
+          return res.json({ user, token });
+        });
+      })(req, res);
+  });
+
+router.get('/api/auth',
+  passport.authenticate('jwt', { session: false }),
+  (req, res, next) => {
+    res.send(req.user);
+  });
+
+module.exports = router;
